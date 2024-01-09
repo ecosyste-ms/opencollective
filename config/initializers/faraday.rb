26763ace410e7e2c90e323415a461c4ab16b755e
Andrew Nesbitt <andrewnez@gmail.com>
2024-01-09 15:39:06 +0000

    Hello world

---
+++ config/initializers/faraday.rb
@@ -0,0 +1,10 @@
+require 'faraday/typhoeus'
+Faraday.default_adapter = :typhoeus
+
+Faraday.default_connection = Faraday::Connection.new do |builder|
+  builder.response :follow_redirects
+  builder.request :url_encoded
+  builder.adapter Faraday.default_adapter
+end
+
+Faraday.default_connection_options = Faraday::ConnectionOptions.new({timeout: 5, open_timeout: 5})

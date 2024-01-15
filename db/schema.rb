868c135ad271e6382eb7873e7c51bd9e5bb4780f
Andrew Nesbitt <andrewnez@gmail.com>
2024-01-15 13:44:08 +0000

    Update schema.rb with new issues_count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_15_101505) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_15_110845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_101505) do
     t.datetime "last_synced_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "issues_count", default: 0
     t.index ["url"], name: "index_projects_on_url", unique: true
   end
 

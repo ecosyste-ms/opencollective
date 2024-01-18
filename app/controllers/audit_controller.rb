e7a3015064ee656b819f183e96485aeedacae7ee
Andrew Nesbitt <andrewnez@gmail.com>
2024-01-18 14:10:17 +0000

    Add audit todos

--- app/controllers/audit_controller.rb
+++ app/controllers/audit_controller.rb
@@ -1,12 +1,14 @@
 class AuditController < ApplicationController
   def index
-        # collectives with no projects
-    # collectives with no open source licenses
-
     @collectives = Collective.where(projects_count: 0).where('balance > 0').order(balance: :desc).select{|c| c.project_url.present?}
 
     @archived = Collective.where(projects_count: 1).select{|c| c.projects.first && c.projects.first.repository && c.projects.first.repository['archived']}
 
     @no_license = Collective.where(projects_count: 1).select{|c| c.projects.first && c.projects.first.repository && !c.projects.first.repository['archived'] && c.projects.first.repository['license'].blank?}
   end
+
+  # collectives where owner is a user instead of an org
+  # collectives with invalid urls
+  # collectives with no projects
+  # collectives with inactive projects
 end

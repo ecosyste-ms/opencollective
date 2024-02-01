0980da0e5f1d5c0c3b806e2e576d75c9535aeb69
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-01 11:40:11 +0000

    Refactor queries in AuditController to order by transactions_count descending and handle null values last

--- app/controllers/audit_controller.rb
+++ app/controllers/audit_controller.rb
@@ -6,30 +6,30 @@ class AuditController < ApplicationController
   end
 
   def user_owners
-    @collectives = Collective.with_user_owner.order(transactions_count: :desc)
+    @collectives = Collective.collectives.with_user_owner.order('transactions_count desc nulls last')
   end
 
   def no_projects
-    @collectives = Collective.where(projects_count: 0).with_transactions.order(transactions_count: :desc).select{|c| c.project_url.present?}
+    @collectives = Collective.collectives.where(projects_count: 0).with_transactions.order('transactions_count desc nulls last').select{|c| c.project_url.present?}
   end
 
   def no_license
-    @collectives = Collective.with_projects.no_license.order(transactions_count: :desc)
+    @collectives = Collective.collectives.with_projects.no_license.order('transactions_count desc nulls last')
   end
 
   def archived
-    @collectives = Collective.with_projects.archived.order(transactions_count: :desc)
+    @collectives = Collective.collectives.with_projects.archived.order('transactions_count desc nulls last')
   end
 
   def inactive
-    @collectives = Collective.with_projects.inactive.order(transactions_count: :desc)
+    @collectives = Collective.collectives.with_projects.inactive.order('transactions_count desc nulls last')
   end
 
   def no_funding
-    @collectives = Collective.with_projects.no_funding.order(transactions_count: :desc)
+    @collectives = Collective.collectives.with_projects.no_funding.order('transactions_count desc nulls last')
   end
 
   def duplicates
-    @collectives = Collective.all.order(transactions_count: :desc).select{|c| c.project_url.present?}.group_by{|c| c.project_url.downcase }.select{|k,v| v.length > 1 }.values.flatten
+    @collectives = Collective.collectives.all.order('transactions_count desc nulls last').select{|c| c.project_url.present?}.group_by{|c| c.project_url.downcase }.select{|k,v| v.length > 1 }.values.flatten
   end
 end

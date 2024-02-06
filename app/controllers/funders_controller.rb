15dd15a724b248941a6ab393ef7526ca30d83362
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-06 16:18:13 +0000

    Refactor FundersController to include only open source transactions

--- app/controllers/funders_controller.rb
+++ app/controllers/funders_controller.rb
@@ -10,10 +10,10 @@ class FundersController < ApplicationController
   def show
     @funder = params[:id]
     @collective = Collective.find_by_slug(@funder)
-    @transactions = Transaction.donations.where(account: @funder).includes(:collective)
+    @transactions = Transaction.opensource.donations.where(account: @funder).includes(:collective)
     raise ActiveRecord::RecordNotFound if @transactions.empty?
     @pagy, @transactions = pagy(@transactions.order(created_at: :desc))
-    @collectives = Transaction.donations.where(account: @funder).group(:collective_id).select('collective_id, SUM(amount) as total').order('total DESC').to_a
+    @collectives = Transaction.opensource.donations.where(account: @funder).group(:collective_id).select('collective_id, SUM(amount) as total').order('total DESC').to_a
     @collective_pagy, @collectives = pagy_array(@collectives.map{|c| c.collective = Collective.find(c.collective_id); c}, page_param: :collective_page)
   end
 end

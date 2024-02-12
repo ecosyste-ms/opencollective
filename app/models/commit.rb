11999d2ef59024e865ccde2446ee4eae567c565a
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-12 17:10:07 +0000

    Add new scopes to Commit model

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,4 +3,11 @@ class Commit < ApplicationRecord
 
   scope :since, ->(date) { where('timestamp > ?', date) }
   scope :until, ->(date) { where('timestamp < ?', date) }
+
+  scope :author, ->(author) { where(author: author) }
+  scope :committer, ->(committer) { where(committer: committer) }
+  scope :merges, -> { where(merge: true) }
+
+  scope :this_period, ->(period) { where('commits.timestamp > ?', period.days.ago) }
+  scope :last_period, ->(period) { where('commits.timestamp > ?', (period*2).days.ago).where('commits.timestamp < ?', period.days.ago) }
 end

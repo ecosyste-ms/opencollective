da3baf2313dab6e7a06f7233c7ead86975ec1e75
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-16 14:05:24 +0000

    Add scope to filter projects without packages

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,6 +19,7 @@ class Project < ApplicationRecord
   scope :keyword, ->(keyword) { where("keywords @> ARRAY[?]::varchar[]", keyword) }
   scope :with_repository, -> { where.not(repository: nil) }
   scope :with_packages, -> { where('length(packages::text) > 2') }
+  scope :without_packages, -> { where('length(packages::text) <= 2') }
 
   scope :package_url, ->(package_url) { where("package_urls @> ARRAY[?]::varchar[]", Project.purl_without_version(package_url)) }
   scope :package_urls, ->(package_urls) { where("package_urls && ARRAY[?]::varchar[]", package_urls.map{|p| Project.purl_without_version(p) }) }

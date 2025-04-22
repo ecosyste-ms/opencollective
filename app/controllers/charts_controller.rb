c7075bea35e5b5c92a380e0d90e45fefd1c890f7
Andrew Nesbitt <andrewnez@gmail.com>
2025-04-22 08:46:20 +0100

    Use fresh_when in charts_controller

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -6,7 +6,7 @@ class ChartsController < ApplicationController
     else
       @transactions = Transaction.opensource
     end
-
+    fresh_when(@transactions, public: true, last_modified: @transactions.maximum(:updated_at))
     render json: Collective.transaction_chart_data(@transactions, kind: params[:chart], period: period, range: range, start_date: start_date, end_date: end_date)
   end
 
@@ -23,6 +23,7 @@ class ChartsController < ApplicationController
     @issues = @issues.human if params[:exclude_bots] == 'true'
     @issues = @issues.bot if params[:only_bots] == 'true'
 
+    fresh_when(@issues, public: true, last_modified: @issues.maximum(:updated_at))
     render json: Collective.issue_chart_data(@issues, kind: params[:chart], period: period, range: range, start_date: start_date, end_date: end_date)
   end
 
@@ -36,6 +37,7 @@ class ChartsController < ApplicationController
       @commits = Commit.all
     end
 
+    fresh_when(@commits, public: true, last_modified: @commits.maximum(:updated_at))
     render json: Collective.commit_chart_data(@commits, kind: params[:chart], period: period, range: range, start_date: start_date, end_date: end_date)
   end
 
@@ -49,6 +51,7 @@ class ChartsController < ApplicationController
       @tags = Tag.all
     end
 
+    fresh_when(@tags, public: true, last_modified: @tags.maximum(:updated_at))
     render json: Collective.tag_chart_data(@tags, kind: params[:chart], period: period, range: range, start_date: start_date, end_date: end_date)
   end
 end
